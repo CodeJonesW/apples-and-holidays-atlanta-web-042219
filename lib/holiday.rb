ca7bc2003426e8c4902d382e5922d7bd31c1e4de
Will Jones <willjones2010@gmail.com>
2019-04-02 17:45:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,9 +61,10 @@ end
 
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
-    holiday.each do |holiday, supplies|
+    holiday.collect do |holiday, supplies|
       if supplies include?("BBQ")
-        return holiday
+        
+        
  end
  end
  end
