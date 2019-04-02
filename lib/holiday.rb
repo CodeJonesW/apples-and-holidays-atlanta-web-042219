43b6e59273c440c54a19448057330ca217707ca9
Will Jones <willjones2010@gmail.com>
2019-04-02 17:48:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,15 +60,14 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  holiday_hash.collect do |season, holiday|
-    holiday.collect do |holiday, supplies|
-      if supplies include?("BBQ")
+  holiday_hash.collect do |season, holidays|
+       holidays.collect do |holiday, supplies|
+      if supplies.include?("BBQ")
         holiday
         end
     end
  end.flatten.compact
 end
-
 
 
 
