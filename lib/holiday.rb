a8eb9d09b463cfdfa8bbdd5068fda9912eac0dad
Will Jones <willjones2010@gmail.com>
2019-04-02 17:36:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,6 +52,7 @@ def all_supplies_in_holidays(holiday_hash)
  holiday_hash.each do |season, holiday|
    puts season.to_s.capitalize! << ":"
    holiday.each do |holiday, supplies|
+     supplies = supplies.join(", ")
      holidays = holiday.to_s.split("_").collect {|x| x.capitalize}.join(" ") << ":"
      puts "  " + holidays + " " + supplies
    end
