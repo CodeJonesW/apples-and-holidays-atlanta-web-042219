df01f273345f70f246e42f2a188753142abf6292
Will Jones <willjones2010@gmail.com>
2019-04-02 17:30:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -20,36 +20,40 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
- holiday_hash[:winter].each do |holiday, decorations|
-   decorations << supply
-end
+  holiday_hash[:winter].each do |holiday, items|
+    items.push(supply)
+    puts items
+  end
 end
 
+
+
 def add_supply_to_memorial_day(holiday_hash, supply)
-  holiday_hash[:spring].each do |holiday, decorations|
-   decorations << supply
-end
+  holiday_hash[:spring][:memorial_day] << supply
+  puts :memorial_day
 end
 
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
+
   holiday_hash[season][holiday_name] = supply_array
   holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  holiday_hash[:winter].values.flatten
-end
+ stuff = holiday_hash[:winter].collect do |holiday, items|
+   items
+ end
+ stuff.flatten
+ end
 
 
 def all_supplies_in_holidays(holiday_hash)
-  holiday_hash.each do |season, holiday|
-    puts season.to_s.capitalize! << ":"
-    holiday.each do |holiday, supplies|
-      supplies = supplies.join(", ")
-      holiday = holiday.to_s.split("_").map {|w| w.capitalize}.join(" ")<< ":"
-      puts "  " + holiday + " " + supplies
-    end
+ holiday_hash.each do |season, holiday|
+   puts season.to_s.capitalize! << ":"
+   holidays.each do |holiday, supplies|
+     puts "#{season}: #{holiday}: #{supplies}"
+   end
   end
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
@@ -61,14 +65,8 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-   holiday_hash.map do |season, holidays|
-    holidays.map do |holiday, items|
-      if items.include?("BBQ")
-         holiday
-      end
-    end
-  end .flatten.compact
-end
+ end
+ 
 
 
 
